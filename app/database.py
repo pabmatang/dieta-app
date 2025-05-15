2b574cef3a28d374bb8470ff5eda1b54de36e356
Pablo Mateos <73099537+pabmatang@users.noreply.github.com>
2025-05-16 01:58:39 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import  sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Asegúrate de usar la URL correcta para tu base de datos
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# Cargar variables de entorno
+load_dotenv()
+
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Asegúrate de usar la URL correcta para tu base de datos
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
